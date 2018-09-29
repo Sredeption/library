22e0c0c3c02900ce7e9e9a5e16debd36685c6823
Sredeption <hjw_redeption@outlook.com>
2018-09-29 17:27:28 +0800

    instrumentation agent

--- src/bftsmart/demo/ycsb/YCSBMessage.java
+++ src/bftsmart/demo/ycsb/YCSBMessage.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2007-2013 Alysson Bessani, Eduardo Alchieri, Paulo Sousa, and the authors indicated in the @author tags
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,9 +25,7 @@ import java.util.HashMap;
 import java.util.Set;
 
 /**
- *
  * @author Marcel Santos
- *
  */
 public class YCSBMessage implements Serializable {
 
@@ -35,11 +33,11 @@ public class YCSBMessage implements Serializable {
 
     public enum Type {
         CREATE, READ, SCAN, UPDATE, DELETE, SIZE, ERROR
-    };
+    }
 
     public enum Entity {
         TABLE, RECORD, FIELD
-    };
+    }
 
     private Type type;
     private Entity entity;
